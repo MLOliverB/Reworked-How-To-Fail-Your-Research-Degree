bcc4254559361360efcaffe6b3188765ec924390
Oliver Billich <2469811b@student.gla.ac.uk>
2022-04-06 14:02:16 +0100

    Added main js script as entry point

---
+++ resources/js/mainGame.js
@@ -0,0 +1,16 @@
+// import Phaser scenes
+
+const config= {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: '#a9e3ff',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        zoom: 1,
+    },
+    scene: []
+}
+
+const game = new Phaser.Game(config);
