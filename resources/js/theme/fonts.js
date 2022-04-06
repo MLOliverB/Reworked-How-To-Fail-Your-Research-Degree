eb3899f1173e1a593455df5699b020607806629a
Oliver Billich <2469811b@student.gla.ac.uk>
2022-04-06 15:17:42 +0100

    #2 - Added font Map
    ## Changes Made
    - Implement font map for commonly used text styles
    - Map taken from original project
    
    Closes #2

---
+++ resources/js/theme/fonts.js
@@ -0,0 +1,10 @@
+const fonts = new Map([
+    ["button",{color: "#000000", fontFamily: "Bahiana", fontSize: "50px"}],
+    ["buttonEvent",{color: "#FFFFFF", fontFamily: "Bahiana", fontSize: "40px"}],
+    ["h1",{color: "#000000", fontFamily: "Bahiana", fontSize: "240px"}],
+    ["h2",{color: "#FFFFFF", fontFamily: "Bahiana", fontSize: "170px"}],
+    ["h3",{color: "#000000", fontFamily: "Bahiana", fontSize: "60px"}],
+    ["h3Light",{color: "#FFFFFF", fontFamily: "Bahiana", fontSize: "60px"}],
+]);
+
+export{ fonts };
