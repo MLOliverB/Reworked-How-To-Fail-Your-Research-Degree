829828b41061e33cd1840f54e25fa222b4e66494
Oliver Billich <2469811b@student.gla.ac.uk>
2022-04-13 18:47:47 +0100

    #21 - Created test files for card data
    ## Changes Made
    - Implemented tests for common properties (title, slug, stage, image)

--- test/testDummy.ts
+++ test/testDummy.ts
@@ -1,14 +1,6 @@
 var assert = require('assert');
 describe('Dummy', function() {
   it('should always pass', function() {
-    assert.equal(1, 1);
+    assert(true);
   });
 });
-
-// describe('Array', function () {
-//   describe('#indexOf()', function () {
-//     it('should return -1 when the value is not present', function () {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
