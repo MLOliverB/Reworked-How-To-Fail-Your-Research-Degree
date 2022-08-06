fb0eb748af84a946c5becf4a517c6eab57af505f
Oliver Billich <2469811b@student.gla.ac.uk>
2022-08-06 17:38:28 +0100

    Added comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: "development", // "production" | "development" | "none"
   entry: path.resolve(__dirname, './bin/dev/mainGame.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
